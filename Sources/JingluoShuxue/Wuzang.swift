c198149203c25bcc7157b93181e2d7039a62e311
Xiangyu Sun <xiangyu.sun@glovoapp.com>
2022-12-09 17:05:51 +0100

    reorder the wuzang

--- Sources/JingluoShuxue/Wuzang.swift
+++ Sources/JingluoShuxue/Wuzang.swift
@@ -8,22 +8,22 @@
 import ChineseAstrologyCalendar
 
 public enum 五臟: String {
+    case 肺
+    case 脾
     case 心
     case 肝
-    case 脾
-    case 肺
     case 腎
     
     public var wuxing: Wuxing {
         switch self {
+        case .肺:
+            return .jin
+        case .脾:
+            return .tu
         case .心:
             return .huo
         case .肝:
             return .mu
-        case .脾:
-            return .tu
-        case .肺:
-            return .jin
         case .腎:
             return .shui
         }
